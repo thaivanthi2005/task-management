7cd4071329f6c7b51670b10cc1347adcd9cba0ee
THÁI VĂN THI <thaivanthi2005@gmail.com>
2026-07-07 00:11:16 +0700

    add sort by criteria

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -6,7 +6,12 @@ module.exports.index = async (req, res) => {
   if (req.query.status) {
     find.status = req.query.status;
   }
-  const tasks = await Task.find(find);
+  console.log(req.query);
+  const sort = {};
+  if (req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue;
+  }
+  const tasks = await Task.find(find).sort(sort);
   res.json(tasks);
 };
 
