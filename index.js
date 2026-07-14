e00faa4f9e8ded8d7dda627f4d01732700088913
THÁI VĂN THI <thaivanthi2005@gmail.com>
2026-07-14 09:39:13 +0700

    fix deloy

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 3000;
 const database = require("./config/database");
 const Task = require("./api/v1/models/task.model");
 const route = require("./api/v1/routes/index.route");
-const { applyDefaults } = require("../begin/models/rooms-chat.model");
 database.connect();
 // parse application/json
 app.use(bodyParser.json());
